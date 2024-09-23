f83bd3a9e0370ba80f9796a7eddd7584dd897007
Ajayss04 <138968836+Ajayss04@users.noreply.github.com>
2024-09-23 22:26:28 +0530

    joke implementation and final steps

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
-    res.render("index.ejs")
+app.get("/", async (req, res)=>{
+    try {
+        const response = await axios.get('https://v2.jokeapi.dev/joke/Any?type=single');
+        res.render("index.ejs", {
+        joke: response.data.joke,
+    })
+    } catch (error) {
+        console.error(error.response.data);
+        res.status(500)
+    }
 })
+
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}.`);
